f4fa7fd5cf177bb4df1a11526691e672bd231a09
Amplu1 <59928949+Amplustomp@users.noreply.github.com>
2020-01-15 17:11:59 -0300

    Update DemoApplication.java

--- src/main/java/com/example/tuvision/DemoApplication.java
+++ src/main/java/com/example/tuvision/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-      return "Hello World!";
+      return "Hello World! - Hi Edu :)";
     }
 
     public static void main(String[] args) {
