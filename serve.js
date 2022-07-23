4ad10281192ed9c7e3a6ce9a9c49c398dfed49eb
Sergio (Admethius) <sergio.cp@hotmail.es>
2022-07-23 16:34:24 -0400

    se supone que funciona

--- serve.js
+++ serve.js
@@ -3,6 +3,15 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/tuvision'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/tuvision/index.html'));});
+res.sendFile(res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}));});
 app.listen(process.env.PORT || 8200);
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
+app.use(express.static('./dist/tuvision.json>'));
