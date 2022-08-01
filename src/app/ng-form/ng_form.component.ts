f6f32e267f7462af651feff6203adf26fb540873
Sergio (Admethius) <sergio.cp@hotmail.es>
2022-08-01 17:55:19 -0400

    se agregan nuevos ejercicios 2

--- src/app/ng-form/ng_form.component.ts
+++ src/app/ng-form/ng_form.component.ts
@@ -11,5 +11,49 @@ export class C03PersonaListarComponent {
 
  //Creamos un Arreglo de Personas, con 4 personas posición de 0 a 3
  public lPersonas:String[] =["Sergio","Roxana","Liliana","Abby"]
+ public lPersonasEliminados:String[]=[]
+
+   // Método que elimina el primer registro del arreglo
+   borrarPrimerPersona(){
+    // shift devuelve string o undefined, por eso colocamos || ''
+    // si es undefined coloque ==> ''
+    const borrado = this.lPersonas.shift() || ''
+    // push agrega al final del registro
+    this.lPersonasEliminados.push(borrado);
+   }
+
+   public booleanA: boolean = true;
+   public booleanB: boolean = true;
+
+   lPersonasPais: any[] = [
+    {
+      "name": "Sergio",
+      "age": 35,
+      "country": 'MARS'
+    },
+    {
+      "name": "Roxana",
+      "age": 32,
+      "country": 'USA'
+    },
+    {
+      "name": "Liliana",
+      "age": 21,
+      "country": 'HK'
+    },
+    {
+      "name": "Abby",
+      "age": 34,
+      "country": 'UK'
+    },
+    {
+      "name": "Locky",
+      "age": 32,
+      "country": 'USA'
+    }
+  ];
+
+  selectedValue: string= 'Two';  //ngSwitch
 
 }
+
